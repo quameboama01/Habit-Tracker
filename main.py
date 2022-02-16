56a468a01d8803c25594a223163f83f2ff1d82e7
Andrews Boama-Secu <quameboama01@gmail.com>
2022-02-16 14:32:16 +0000

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("This is a new habit tracker script")
-print("The rally ofload ")
+import re
+import requests
+html_text = requests.get("https://www.google.com")
+html_text.raise_for_status()
+stat_code = html_text.status_code
+if stat_code == 200:
+    print("Yaay!, We have connected to the Server")
+else:
+    print("Connection lost, Please try again later.")
