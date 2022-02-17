ddb19440f68a50af90d7fd31ed63ad772663b781
Andrews Boama-Secu <quameboama01@gmail.com>
2022-02-17 11:45:00 +0000

    graph commit

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import re
 import requests
-html_text = requests.get("https://www.google.com")
-html_text.raise_for_status()
-stat_code = html_text.status_code
-if stat_code == 200:
-    print("Yaay!, We have connected to the Server")
-else:
-    print("Connection lost, Please try again later.")
-print("\nThe End")
+
+TOKEN = "jadjfhkjafahjajajjafk"
+USERNAME = "andrews"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+    }
+graph_endpoint = f"{pixela_endpoint}{USERNAME}/graphs"
+print(graph_endpoint)
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "shibafu"
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config)
+response.raise_for_status
+print(response.text)
+
