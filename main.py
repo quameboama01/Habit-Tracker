5a04e6501e562e868a03e8ed136f0d1d9902ce63
Andrews Boama-Secu <quameboama01@gmail.com>
2022-02-17 13:23:47 +0000

    pre final commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
+import datetime as dt
 
 TOKEN = "jadjfhkjafahjajajjafk"
 USERNAME = "andrews"
+GRAPH_ID = "graph1"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -10,8 +13,7 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
     }
-graph_endpoint = f"{pixela_endpoint}{USERNAME}/graphs"
-print(graph_endpoint)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
     "id": "graph1",
@@ -20,8 +22,19 @@ graph_config = {
     "type": "float",
     "color": "shibafu"
 }
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
 
-response = requests.post(url=graph_endpoint, json=graph_config)
+now = dt.datetime.now()
+date = now.strftime("%Y%m%d")
+
+pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+pixel_details = {
+    "date": date,
+    "quantity": input("How many Kilometers did you Cycle today? ")
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_details, headers=headers)
 response.raise_for_status
 print(response.text)
-
